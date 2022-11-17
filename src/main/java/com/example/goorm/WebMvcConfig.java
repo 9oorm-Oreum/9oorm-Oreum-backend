ac4d6ca6d39c462ece88958b2f5492ce21dd7616
Song-EunJu <jd06280@naver.com>
2022-11-17 11:50:39 +0900

    add: cors 요청 허용

---
+++ src/main/java/com/example/goorm/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.goorm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://9oorm-oreum-frontend.vercel.app/")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
