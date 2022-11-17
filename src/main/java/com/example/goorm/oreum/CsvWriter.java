ff6fab6e4c78ca004e35ecc2601b84634096ad47
Song-EunJu <jd06280@naver.com>
2022-11-17 18:13:23 +0900

    del: 필요없는 파일 삭제

--- src/main/java/com/example/goorm/oreum/CsvWriter.java
+++ src/main/java/com/example/goorm/oreum/CsvWriter.java
@@ -1,21 +1,21 @@
-package com.example.goorm.oreum;
-
-import com.example.goorm.oreum.repository.OreumRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Configuration
-@RequiredArgsConstructor
-public class CsvWriter implements ItemWriter<Oreum> {
-
-    private final OreumRepository oreumRepository;
-
-    @Override
-    public void write(List<? extends Oreum> list) throws Exception {
-        oreumRepository.saveAll(new ArrayList<Oreum>(list));
-    }
-}
+//package com.example.goorm.oreum;
+//
+//import com.example.goorm.oreum.repository.OreumRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.item.ItemWriter;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class CsvWriter implements ItemWriter<Oreum> {
+//
+//    private final OreumRepository oreumRepository;
+//
+//    @Override
+//    public void write(List<? extends Oreum> list) throws Exception {
+//        oreumRepository.saveAll(new ArrayList<Oreum>(list));
+//    }
+//}
